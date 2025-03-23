4d02c185fff1ea4109b98f64ededfd3c4b3e912f
4OH4Loop <semperchuck@gmail.com>
2025-03-23 16:23:46 -0400

    Ch 6.10 Updates

--- CatchEmAll/CreaturesListView.swift
+++ CatchEmAll/CreaturesListView.swift
@@ -9,25 +9,21 @@ import SwiftUI
 
 struct CreaturesListView: View {
     @State var creatures = Creatures()
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack {
-            List(creatures.creaturesArray) { creature in
+            List(searchResults) { creature in
                 LazyVStack {
                     NavigationLink {
                         DetailView(creature: creature)
                     } label: {
-                        Text(creature.name.capitalized)
+                        Text("\(returnIndex(of: creature)). \(creature.name.capitalized)")
                             .font(.title2)
                     }
                 }
                 .task {
-                    guard let lastCreature = creatures.creaturesArray.last else { return
-                    }
-                    if creature.name == lastCreature.name &&
-                        creatures.urlString.hasPrefix("http") {
-                        await creatures.getData()
-                    }
+                    await creatures.loadNextIfNeeded(creature: creature)
                 }
             }
             .listStyle(.plain)
@@ -44,6 +40,8 @@ struct CreaturesListView: View {
                     Text("\(creatures.creaturesArray.count) of \(creatures.count) creatures")
                 }
             }
+            .searchable(text: $searchText)
+            
             if creatures.isLoading {
                 ProgressView()
                     .tint(.red)
@@ -53,6 +51,18 @@ struct CreaturesListView: View {
         .task {
             await creatures.getData()
         }
+    }
+    var searchResults: [Creature] {
+        if searchText.isEmpty {
+            return creatures.creaturesArray
+        } else {
+            return creatures.creaturesArray.filter {$0.name.capitalized.contains(searchText)}
+        }
+    }
+    
+    func returnIndex(of creature: Creature) -> Int {
+        guard let index = creatures.creaturesArray.firstIndex(where: {$0.name == creature.name}) else {return 0}
+        return index + 1
     }
 }
 
